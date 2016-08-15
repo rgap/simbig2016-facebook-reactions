510719d41736105cfd7425d223ee6ed9c62d79f1
rgap <r.guzmanap@gmail.com>
2016-08-15 03:49:24 -0500

    done

--- 1_facebook_fanpage_scrapper/facebook_scrapper.py
+++ 1_facebook_fanpage_scrapper/facebook_scrapper.py
@@ -1,22 +1,20 @@
 import re
-import urllib2
-import urllib
 import json
-import time
 import sys
-import datetime
-from dateutil.relativedelta import relativedelta
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from urllib.parse import unquote_plus
 
 
 class FacebookScrapper:
 
     GRAPH_URL = "https://graph.facebook.com/"
-    DB_POST_ATRIBUTES = ["id", "fb_id", "message", "likes_count", "love_count",
-                         "wow_count", "haha_count", "sad_count", "angry_count",
-                         "thankful_count", "time_created", "shares",
+    DB_POST_ATRIBUTES = ["id", "fb_id", "message", "fb_like", "fb_love",
+                         "fb_wow", "fb_haha", "fb_sad", "fb_angry",
+                         "fb_thankful", "time_created", "shares",
                          "fanpagelink", "type", "link", "name", "description",
                          "external_picture", "num_comments", "page_id"]
-    DB_COMMENT_ATRIBUTES = ["id", "fb_id", "message", "likes_count",
+    DB_COMMENT_ATRIBUTES = ["id", "fb_id", "message", "fb_like",
                             "time_created", "post_id"]
 
     def __init__(self, APP_SECRET, APP_ID):
@@ -30,9 +28,9 @@ class FacebookScrapper:
         tries = 1
         while tries <= 20:
             try:
-                web_response = urllib2.urlopen(json_url, timeout=30)
+                web_response = urlopen(json_url, timeout=50)
                 readable_page = web_response.read()
-                json_data = json.loads(readable_page)
+                json_data = json.loads(readable_page.decode("utf-8"))
                 return json_data
 
             except:
@@ -40,9 +38,8 @@ class FacebookScrapper:
                 print(json_url)
                 tries += 1
 
-        key = raw_input("Probably the number of requests was exceeded, "
-                        "(1) stop, (2) keep trying : ")
-
+        key = input("Probably the number of requests was exceeded, "
+                    "(1) stop, (2) keep trying : ")
         if key == "1":
             print("stop")
             return "L"
@@ -91,13 +88,14 @@ class FacebookScrapper:
             c[6] = likes_json["reactions_thankful"]["summary"]["total_count"]
             return c
         except:
-            print("exception: get_likes_count")
+            print("exception: get_reactions_count")
             print(likes_url)
             return ""
 
     def create_post_url(self, company):
         # Create authenticated post URL
-        post_args = "/posts/?key=value&access_token=" + self.APP_ID + "|" + self.APP_SECRET
+        post_args = ("/posts/?key=value"
+                     "&access_token=" + self.APP_ID + "|" + self.APP_SECRET)
         post_url = self.GRAPH_URL + company + post_args
 
         return post_url
@@ -105,7 +103,9 @@ class FacebookScrapper:
     def get_picture_url(self, url):
         try:
             url_parsed = urlparse.parse_qs(urlparse.urlparse(url).query)["url"]
-            return re.sub(r'\\(.)', r'\1', urllib.unquote_plus(url_parsed.decode('unicode_escape')))
+            return re.sub(r'\\(.)', r'\1',
+                          unquote_plus(
+                            url_parsed.decode('unicode_escape')))
         except:
             return ""
         finally:
@@ -168,13 +168,13 @@ class FacebookScrapper:
                 counter = self.get_reactions_count(fb_post_id)
                 if counter == "L":
                     return
-                likes_count = counter["likes"]
-                love_count = counter["love"]
-                wow_count = counter["wow"]
-                haha_count = counter["haha"]
-                sad_count = counter["sad"]
-                angry_count = counter["angry"]
-                thankful_count = counter["thankful"]
+                fb_like = counter[0]
+                fb_love = counter[1]
+                fb_wow = counter[2]
+                fb_haha = counter[3]
+                fb_sad = counter[4]
+                fb_angry = counter[5]
+                fb_thankful = counter[6]
 
                 time_created = self.get_elementvalue(post, "created_time")
                 shares = self.get_elementvalue(post, ["shares", "count"])
@@ -189,36 +189,37 @@ class FacebookScrapper:
                 if num_comments == "L":
                     return
 
-                current_post = [fb_post_id, message, likes_count, love_count,
-                                wow_count, haha_count, sad_count, angry_count,
-                                thankful_count, time_created, shares,
+                current_post = [fb_post_id, message, fb_like, fb_love,
+                                fb_wow, fb_haha, fb_sad, fb_angry,
+                                fb_thankful, time_created, shares,
                                 fanpagelink, post_type, link, name,
                                 description, external_picture, num_comments]
 
                 len_posts = len(posts)
-                if len_posts <= max_posts and date <= time_created:
+                if len_posts < max_posts and date <= time_created:
                     posts.append(current_post)
-                    print "post # " + str(len_posts)
+                    print("post # " + str(len_posts))
                 else:
                     return
 
         except Exception:
-            print 'fatal error: after getting posts - scrape_posts_by_date'
+            print("fatal error: after getting posts - scrape_posts_by_date")
             return
 
         try:
             # Extract next page
             next_page = json_data["paging"]["next"]
-            print "next page = " + next_page
+            print("next page = " + next_page)
             self.scrape_posts_by_date(next_page, date, posts, company,
                                       db_posts_dict, max_posts)
         except:
-            print "no more posts"
+            print("no more posts")
             return
 
     def create_comments_url(self, post_id):
         comments_args = (post_id + "/comments/?key=value&summary=true"
-                         "&access_token=" + self.APP_ID + "|" + self.APP_SECRET)
+                         "&access_token=" +
+                         self.APP_ID + "|" + self.APP_SECRET)
         comments_url = self.GRAPH_URL + comments_args
         return comments_url
 
@@ -232,8 +233,8 @@ class FacebookScrapper:
                 return "L"
             return comments_json["summary"]["total_count"]
         except:
-            print "exception: get_num_comments"
-            print comments_url
+            print("exception: get_num_comments")
+            print(comments_url)
             return ""
 
     def get_comments_data(self, comments_url, comments, post_id):
@@ -251,19 +252,19 @@ class FacebookScrapper:
                             self.get_elementvalue(comment, "id"))
                 comment_message = (
                             self.get_elementvalue(comment, "message"))
-                comment_like_count = (
-                            self.get_elementvalue(comment, "like_count"))
+                comment_fb_like = (
+                            self.get_elementvalue(comment, "fb_like"))
                 comment_created_time = (
                             self.get_elementvalue(comment, "created_time"))
                 current_comment = [comment_id, comment_message,
-                                   comment_like_count, comment_created_time,
+                                   comment_fb_like, comment_created_time,
                                    post_id]
                 comments.append(current_comment)
 
-            print "comments # " + str(len(comments))
+            print("comments # " + str(len(comments)))
             # Extract next page
             next_page = json_data["paging"]["next"]
-            print "next page"
+            print("next page")
             self.get_comments_data(next_page, comments, post_id)
 
         except Exception:
--- 1_facebook_fanpage_scrapper/main.py
+++ 1_facebook_fanpage_scrapper/main.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+"""Image file resizer
+
+Usage:
+    main.py <max_posts>
+
+Arguments:
+    max_posts   maximum number of posts to get
+
+"""
+
+import sys
+import json
 import mysql.connector
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -14,116 +27,169 @@ def get_db_posts(db_posts_dict, db_posts):
 
 class DBConnection:
 
-    def __init__(self):
+    def __init__(self, db_user, db_password, hostname, database):
         # Fill this out with your db connection info
-        self.connection = mysql.connector. \
-                          connect(user='root', password='1',
-                                  host='localhost',
-                                  database='facebook_fanpage_unilad')
-        self.cursor = self.connection.cursor()
+        self.db_user = db_user
+        self.db_password = db_password
+        self.hostname = hostname
+        self.database = database
 
         self.query_selectid_page = ("SELECT id FROM page WHERE fb_id = %s")
         # SQL statement for adding Facebook page data to database
         self.query_insert_page = ("INSERT INTO page "
-                                  "(fb_id, likes, talking_about, "
+                                  "(fb_id, fb_like, talking_about, "
                                   "username, about, description) "
                                   "VALUES (%s, %s, %s, %s, %s, %s)")
 
         self.query_selectid_post = ("SELECT id FROM post WHERE fb_id = %s")
         # SQL statement for adding post data
         self.query_insert_post = ("INSERT INTO post "
-                                  "(fb_id, message, likes_count, "
+                                  "(fb_id, message, fb_like, "
+                                  "fb_love, fb_wow, fb_haha, "
+                                  "fb_sad, fb_angry, fb_thankful, "
                                   "time_created, shares, fanpagelink, type, "
                                   "link, name, description, external_picture, "
-                                  "num_comments, page_id)"
+                                  "num_comments, page_id) "
                                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, "
-                                  "%s, %s, %s, %s, %s)")
+                                  "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                                  )
 
         self.query_selectid_comment = ("SELECT id FROM comment "
                                        "WHERE fb_id = %s")
         # SQL statement for adding comment data
         self.query_insert_comment = ("INSERT INTO comment "
-                                     "(fb_id, message, likes_count, "
+                                     "(fb_id, message, fb_like, "
                                      "time_created, post_id)"
                                      "VALUES (%s, %s, %s, %s, %s)")
 
+    def connect(self):
+        self.connection = mysql.connector. \
+                          connect(user=self.db_user, password=self.db_password,
+                                  host=self.hostname, database=self.database)
+        self.cursor = self.connection.cursor()
+        print("connection established")
+
     def close(self):
-        print("connection closed")
         self.connection.close()
+        print("connection closed")
 
     def commit(self):
-        print("data committed")
         # Commit the data to the db
         self.connection.commit()
+        print("data committed")
 
     def insert_info(self, page):
         try:
             self.cursor.execute(self.query_insert_page, page)
             return self.cursor.lastrowid
         except:
-            while True:
+            tries = 1
+            while tries <= 20:
                 try:
                     self.cursor.execute(self.query_selectid_page, [page[0]])
                     print("insert_info already exists")
                     return self.cursor.fetchone()[0]
-                except:
-                    print("no mysql connection")
+                except mysql.connector.Error as e:
+                    print("Something went wrong - insert_info: {}".format(e))
+                    print("trying again... " + str(tries))
+                    tries += 1
                     pass
+        key = input("Probably lost connection with mysql "
+                    "(1) stop, (2) keep trying : ")
+        if key == "1":
+            print("stop")
+            sys.exit()
+        if key == "2":
+            print("keep trying")
+            self.insert_info(page)
 
     def insert_post(self, post):
         try:
             self.cursor.execute(self.query_insert_post, post)
             return self.cursor.lastrowid
-        except:
-            while True:
+        except mysql.connector.Error as e:
+            print("Not inserted: {}".format(e))
+            tries = 1
+            while tries <= 20:
                 try:
                     self.cursor.execute(self.query_selectid_post, [post[0]])
                     print("insert_post already exists")
                     return self.cursor.fetchone()[0]
-                except:
-                    print("no mysql connection")
+                except mysql.connector.Error as e:
+                    print("Something went wrong - insert_post: {}".format(e))
+                    print("trying again... " + str(tries))
+                    tries += 1
                     pass
+        key = input("Probably lost connection with mysql "
+                    "(1) stop, (2) keep trying : ")
+        if key == "1":
+            print("stop")
+            sys.exit()
+        if key == "2":
+            print("keep trying")
+            self.insert_post(post)
 
     def insert_comment(self, comment):
         try:
             self.cursor.execute(self.query_insert_comment, comment)
             return self.cursor.lastrowid
         except:
-            while True:
+            tries = 1
+            while tries <= 20:
                 try:
                     self.cursor.execute(self.query_selectid_comment,
                                         [comment[0]])
                     print("insert_comment already exists")
                     return self.cursor.fetchone()[0]
-                except:
-                    print("no mysql connection")
+                except mysql.connector.Error as e:
+                    print("Something went wrong - insert_comment: {}".format(e))
+                    print("trying again... " + str(tries))
+                    tries += 1
                     pass
+        key = input("Probably lost connection with mysql "
+                    "(1) stop, (2) keep trying : ")
+        if key == "1":
+            print("stop")
+            sys.exit()
+        if key == "2":
+            print("keep trying")
+            self.insert_comment(comment)
 
     def get_posts(self):
         self.cursor.execute("SELECT * FROM post")
         return self.cursor.fetchall()
 
 
-def main():
+def main(args):
+
+    # Max number of posts
+    max_posts = int(args['<max_posts>'])
+
+    # Load configuration file
+    with open('config.json') as config_file:
+        config = json.loads(config_file.read())
 
     # To find go to page's FB page, at the end of URL find username
     # E.g. http://facebook.com/walmart, walmart is the username
-    list_companies = ["uniladmag"]
+    list_companies = [config["fb_fanpage_name"]]
 
     # Save all comments
     save_comments = False
-    # Max number of posts
-    max_posts = 10
 
     # The time of last weeks crawl
     last_crawl = datetime.datetime.now() - relativedelta(years=200)
     last_crawl = last_crawl.isoformat()
 
-    db_connection = DBConnection()
+    # Configure connection and connect
+    db_connection = DBConnection(config["db_user"],
+                                 config["db_password"],
+                                 config["hostname"],
+                                 config["database"])
+    db_connection.connect()
 
     # Simple data pull App Secret and App ID
-    APP_SECRET = "28578690baddda5067c19cd7b7c9446e"
-    APP_ID = "1443249892642264"
+    APP_SECRET = config["APP_SECRET"]
+    APP_ID = config["APP_ID"]
     fc = FacebookScrapper(APP_SECRET, APP_ID)
 
     # Get posts already in database
@@ -135,9 +201,10 @@ def main():
 
         # Make graph api url with company username
         page = fc.get_page_info(company)
-        # Insert the data we pulled into db and commit
+        # Insert the data we pulled into db, commit and close connection
         page_id = db_connection.insert_info(page)
         db_connection.commit()
+        db_connection.close()
 
         # Extract post data
         post_url = fc.create_post_url(company)
@@ -146,6 +213,8 @@ def main():
         fc.scrape_posts_by_date(post_url, last_crawl,
                                 posts, company, db_posts_dict, max_posts)
 
+        # Reconnect
+        db_connection.connect()
         # Loop through and insert posts
         for post in posts:
             print("storing post data......... " + post[0])
@@ -162,10 +231,8 @@ def main():
                 for comment in comments:
                     db_connection.insert_comment(comment)
                     db_connection.commit()
-
-        # Commit the data to the db
-        db_connection.commit()
     db_connection.close()
 
 if __name__ == "__main__":
-    main()
+    from docopt import docopt
+    main(docopt(__doc__))
